8865c14c9f6229b48631f46a154a9d107c3579e9
hari042 <hariprasada1998@gmail.com>
2021-07-28 15:32:42 +0530

    Final Commit

--- WebContent/script/include.js
+++
@@ -1,8 +0,0 @@
-
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'home'+ $(this).data('include') + 'home.html'
-      $(this).load(file)
-    })
-  })
