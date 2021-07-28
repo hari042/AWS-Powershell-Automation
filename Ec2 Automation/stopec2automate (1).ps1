750b1b68108d1bad1b88a779a80bed86cfb021bf
hari042 <hariprasada1998@gmail.com>
2021-07-28 14:56:47 +0530

    Final Commit

---
+++ Ec2 Automation/stopec2automate (1).ps1
@@ -0,0 +1,14 @@
+﻿$taskAction = New-ScheduledTaskAction `
+    -Execute 'C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe' `
+    -Argument '-File c:\acm\stopec2instance.ps1'
+$taskAction
+
+$taskTrigger = New-ScheduledTaskTrigger -Daily -At 9:00PM
+$tasktrigger
+$taskName = "Ec2stopping"
+$description = "stop the ec2 instance"
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description
